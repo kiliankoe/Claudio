e226985c273087c137c1cb069455b1319dac3d8b
Kilian Koeltzsch <me@kilian.io>
2020-05-25 17:52:24 +0200

    Initial Commit

---
+++ Claudio/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Claudio
+//
+//  Created by Kilian Koeltzsch on 25.05.20.
+//  Copyright © 2020 Kilian Koeltzsch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
